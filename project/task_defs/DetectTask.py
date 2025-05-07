b2d340e787d28449dc22178c632dae860bc9fb94
xianglic <xianglic@andrew.cmu.edu>
2025-05-07 12:15:59 -0400

    testing subtree push from monorepo

--- project/task_defs/DetectTask.py
+++ project/task_defs/DetectTask.py
@@ -53,7 +53,7 @@ class DetectTask(Task):
     @Task.call_after_exit
     async def run(self):
         # init the data
-        logger.info("test, for pullin32")
+        logger.info("test, for pullin3")
         model = self.task_attributes["model"]
         lower_bound = self.task_attributes["lower_bound"]
         upper_bound = self.task_attributes["upper_bound"]
